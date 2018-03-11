f712ba52c40e206fd578c9fd21d65eb9ac6e1b0d
Jairo Daniel <al221211221@gmail.com>
2018-03-10 22:20:28 -0600

    se agrego la primer seguridad

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 class FrontController extends Controller
 {
     public function __invoke(){
+        if (!session('status')){
+            return redirect()->route('home');
+        }
         return view('login');
     }
 }
